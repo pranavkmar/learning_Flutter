3d3654125e5b43217584aae8ea0a27adbec479f7
ISEA <informpranav@gmail.com>
2020-01-08 16:52:13 +0530

    add assetImage

--- lib/ui/firstHome.dart
+++ lib/ui/firstHome.dart
@@ -1,37 +1,61 @@
 import 'dart:math';
-import 'uiWidgets.dart' as UIWid;
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'uiWidgets.dart' as UIWid;
 
 class HomeFirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-        color: Colors.lightBlueAccent,
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: Text(
-                generateLuckyNumber(),
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.white, fontSize: 32.0),
-              ),
-            ),
-            RaisedButton(
-              child: Text('Open UI Widget'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => UIWid.uiWidgets()),
-                );
-              },
-            ),
-          ],
-        ));
+    return Center(
+      child: Material(
+        child: Container(
+            padding: EdgeInsets.all(20.0),
+            alignment: Alignment.center,
+            color: Colors.lightBlueAccent,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          generateLuckyNumber(),
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(color: Colors.white, fontSize: 36.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Row(
+                    children: <Widget>[
+                      RaisedButton(
+                        child: Text('Open UI Widget'),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => UIWid.UiWidgets()),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )),
+      ),
+    );
   }
 }
 
 String generateLuckyNumber() {
   var random = Random();
   int luckyNumber = random.nextInt(10);
-  return " Your Lucky Number is $luckyNumber";
+  return "Your Lucky Number is $luckyNumber";
 }
--- lib/ui/uiWidgets.dart
+++ lib/ui/uiWidgets.dart
@@ -1,23 +1,67 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class uiWidgets extends StatelessWidget {
+class UiWidgets extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.deepPurple,
-      child: Column(
-        children: <Widget>[
-          Text("Flight", textDirection: TextDirection.ltr),
-          RaisedButton(
-            child: Text("Go to Home"),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          )
-        ],
+    return Center(
+      child: Material(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('UI Scanner'),
+          ),
+          body: Container(
+            padding: EdgeInsets.all(20.0),
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "Flight",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Row(
+                    children: <Widget>[
+                      RaisedButton(
+                        child: Text("Go to Home"),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              FlightImageAsset(),
+
+              ],
+            ),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class FlightImageAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/flight.png');
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
     );
   }
 }
